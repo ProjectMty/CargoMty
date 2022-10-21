f83b54d48e19724078f2a749f2f1854159a24843
Luis R. Carrillo <luisr_carrillo@outlook.com>
2022-10-20 23:05:43 -0500

    ✏️ : ecommerce typo

--- src/i18n/sections/es/ecommerce.tsx
+++ src/i18n/sections/es/ecommerce.tsx
@@ -4,8 +4,7 @@ const Ecommerce = () => {
   return (
     <section id='ecommerce' className='ecommerce-separator usa-ecommerce'>
       <h2 className='text-base font-normal uppercase text-white lg:text-2xl 2xl:text-3xl'>
-        <strong>Conoce nuestro</strong> servicio
-        <br className='lg:hidden' />
+        <strong>Conoce nuestro</strong> servicio <br className='lg:hidden' />{' '}
         para <strong>Estados Unidos</strong>
       </h2>
       <Link href='/' locale='en'>
