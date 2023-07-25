6b283e08cb98d92c9f74971f9a715ee5cbfdaf19
Luis R. Carrillo <luisr_carrillo@outlook.com>
2023-07-25 13:21:44 -0500

    Google Tags (Scripts)

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import {
 } from '@/src/sections';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import Script from 'next/script';
 
 const Home: NextPage = () => {
   return (
@@ -36,6 +37,21 @@ const Home: NextPage = () => {
         <Contacto />
         <Footer />
       </Drawer>
+      <div className='container'>
+        <Script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=G-LEDG07K52F'
+        />
+        <Script id='google-analytics'>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-LEDG07K52F');
+        `}
+        </Script>
+      </div>
     </>
   );
 };
