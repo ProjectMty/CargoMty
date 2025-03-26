d1ead9756cf2c348c088de0c3134a8b010945c19
Issac <Soporte@cargomty.com>
2025-03-26 17:01:14 -0600

    cambio de numero

--- src/sections/contacto.tsx
+++ src/sections/contacto.tsx
@@ -13,7 +13,7 @@ const Contacto = () => {
         }}
         phone='+52 81 2090 3977'
         email='info@cargomty.com'
-        whatsapp={['+52 81 1669 1037', '+52 81 8176 6691', '+52 81 1599 5077']}
+        whatsapp={['+52 81 1669 1037', '+52 81 8176 6691', '+52 1 81 1412 3816']}
         btnMessage='Enviar'
         nameInput={{ label: 'Nombre', placeholder: 'Nombre' }}
         phoneInput={{ label: 'Teléfono', placeholder: 'Teléfono' }}
