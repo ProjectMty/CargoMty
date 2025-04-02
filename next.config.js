f9fa35374bb4c555b73fba562c40905b01ee0cf4
Issac <Soporte@cargomty.com>
2025-04-02 16:13:31 -0600

    banner 2.0

--- next.config.js
+++ next.config.js
@@ -1,9 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['cargomty.com'], // Reemplaza esto con el dominio de donde provienen tus imágenes externas
+    domains: ['cargomty.com'], // Asegúrate de que el dominio esté correcto
   },
   reactStrictMode: true,
+  // Agregar configuración para la compilación optimizada en Netlify si es necesario
+  output: 'standalone', // Esto puede ser útil para Netlify
+  webpack: (config, { isServer }) => {
+    // Si deseas personalizar la configuración de Webpack
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
