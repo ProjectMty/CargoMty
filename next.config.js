4ec4eb663d1a4707df2e75a624c1eab4908e2a18
Issac <Soporte@cargomty.com>
2025-04-02 12:17:10 -0600

    banner1.0

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['cargomty.com'], // Reemplaza esto con el dominio de donde provienen tus imágenes externas
+  },
+  reactStrictMode: true,
+};
 
 module.exports = nextConfig;
